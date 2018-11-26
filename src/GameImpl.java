55300c6b3318049cced558f0a3aae243e020d39c
praneetmek <praneetmek@gmail.com>
2018-11-25 21:45:27 -0500

    aa

--- src/GameImpl.java
+++ src/GameImpl.java
@@ -29,16 +29,18 @@ public class GameImpl extends Pane implements Game {
 	 */
 	public static final int HEIGHT = 600;
 
+	private final double SPEED_FACTOR=1.5;
+
 	// Instance variables
 	private Ball ball;
 	private Paddle paddle;
 
 	private int numberOfBottomHits;
-	private int numberOfAnimalsLeft;
+	private ArrayList<ImageView> animals;
 
 	private boolean collisionInFrame;
 
-	ImageView iv1;
+	private ImageView iv1,iv2,iv3,iv4, iv5, iv6, iv7, iv8,iv9,iv10,iv11,iv12,iv13,iv14,iv15,iv16;
 
 	/**
 	 * Constructs a new GameImpl.
@@ -64,15 +66,128 @@ public class GameImpl extends Pane implements Game {
 		ball = new Ball();
 		getChildren().add(ball.getCircle());  // Add the ball to the game board
 
-		numberOfAnimalsLeft=1;
+		animals=new ArrayList<ImageView>();
 
 		// Create and add animals ...
-		Image imageOne=new Image("horse.jpg");
+		Image horse = new Image("horse.jpg");
+		Image duck = new Image("duck.jpg");
+		Image goat = new Image("goat.jpg");
+
 		iv1=new ImageView();
-		iv1.setImage(imageOne);
+		iv1.setImage(horse);
 		iv1.setX(40);
-		iv1.setY(40);
+		iv1.setY(20);
 		getChildren().add(iv1);
+		animals.add(iv1);
+
+
+		iv2=new ImageView();
+		iv2.setImage(duck);
+		iv2.setX(130);
+		iv2.setY(20);
+		getChildren().add(iv2);
+		animals.add(iv2);
+
+
+		iv3=new ImageView();
+		iv3.setImage(goat);
+		iv3.setX(220);
+		iv3.setY(20);
+		getChildren().add(iv3);
+		animals.add(iv3);
+
+
+		iv4=new ImageView();
+		iv4.setImage(horse);
+		iv4.setX(310);
+		iv4.setY(20);
+		getChildren().add(iv4);
+		animals.add(iv4);
+
+		iv5=new ImageView();
+		iv5.setImage(duck);
+		iv5.setX(40);
+		iv5.setY(80);
+		getChildren().add(iv5);
+		animals.add(iv5);
+
+		iv6=new ImageView();
+		iv6.setImage(goat);
+		iv6.setX(130);
+		iv6.setY(80);
+		getChildren().add(iv6);
+		animals.add(iv6);
+
+
+		iv7=new ImageView();
+		iv7.setImage(horse);
+		iv7.setX(220);
+		iv7.setY(80);
+		getChildren().add(iv7);
+		animals.add(iv7);
+
+		iv8=new ImageView();
+		iv8.setImage(duck);
+		iv8.setX(310);
+		iv8.setY(80);
+		getChildren().add(iv8);
+		animals.add(iv8);
+
+		iv9=new ImageView();
+		iv9.setImage(goat);
+		iv9.setX(40);
+		iv9.setY(140);
+		getChildren().add(iv9);
+		animals.add(iv9);
+
+		iv10=new ImageView();
+		iv10.setImage(horse);
+		iv10.setX(130);
+		iv10.setY(140);
+		getChildren().add(iv10);
+		animals.add(iv10);
+
+		iv11=new ImageView();
+		iv11.setImage(duck);
+		iv11.setX(220);
+		iv11.setY(140);
+		getChildren().add(iv11);
+		animals.add(iv11);
+
+		iv12=new ImageView();
+		iv12.setImage(goat);
+		iv12.setX(310);
+		iv12.setY(140);
+		getChildren().add(iv12);
+		animals.add(iv12);
+
+		iv13=new ImageView();
+		iv13.setImage(horse);
+		iv13.setX(40);
+		iv13.setY(200);
+		getChildren().add(iv13);
+		animals.add(iv13);
+
+		iv14=new ImageView();
+		iv14.setImage(duck);
+		iv14.setX(130);
+		iv14.setY(200);
+		getChildren().add(iv14);
+		animals.add(iv14);
+
+		iv15=new ImageView();
+		iv15.setImage(goat);
+		iv15.setX(220);
+		iv15.setY(200);
+		getChildren().add(iv15);
+		animals.add(iv15);
+
+		iv16=new ImageView();
+		iv16.setImage(horse);
+		iv16.setX(310);
+		iv16.setY(200);
+		getChildren().add(iv16);
+		animals.add(iv16);
 
 
 		// Create and add paddle
@@ -167,21 +282,56 @@ public class GameImpl extends Pane implements Game {
 		return isCollisionWithPaddle;
 	}
 	private boolean collisionWithSide(ImageView iv){
-		double topBall = ball.getY() - ball.getCircle().getRadius();
-		double bottomBall = ball.getY() + ball.getCircle().getRadius();
-		double leftBall=ball.getX()-ball.getCircle().getRadius();
-		double rightBall=ball.getX()+ball.getCircle().getRadius();
+		if(getChildren().contains(iv)) {
+			double topBall = ball.getY() - ball.getCircle().getRadius();
+			double bottomBall = ball.getY() + ball.getCircle().getRadius();
+			double leftBall = ball.getX() - ball.getCircle().getRadius();
+			double rightBall = ball.getX() + ball.getCircle().getRadius();
 
-		double topIV=iv.getY();
-		double botIV=topIV+iv.getImage().getHeight();
-		double leftIV=iv.getX();
-		double rightIV=leftIV+iv.getImage().getWidth();
+			double topIV = iv.getY();
+			double botIV = topIV + iv.getImage().getHeight();
+			double leftIV = iv.getX();
+			double rightIV = leftIV + iv.getImage().getWidth();
+			boolean inRow = ((topBall >= topIV && topBall <= botIV) || (bottomBall <= botIV && bottomBall >= topIV));
 
-		boolean touchingLeft = (leftBall<rightIV && leftBall>leftIV) &&
-				((topBall>topIV && topBall<botIV) || (bottomBall<botIV && bottomBall>topIV));
-		boolean touchingRight = (rightBall<rightIV && rightBall>leftIV) &&
-				((topBall>topIV && topBall<botIV) || (bottomBall<botIV && bottomBall>topIV));
-		return touchingLeft||touchingRight;
+			boolean touchingLeft = leftBall <= rightIV && leftBall >= leftIV && rightBall>rightIV && inRow && ball.getX()<leftIV;
+			boolean touchingRight = (rightBall <= rightIV && rightBall >= leftIV&&leftBall<leftIV) && inRow && ball.getX()>rightIV;
+			if (touchingLeft || touchingRight) {
+				getChildren().remove(iv);
+				animals.remove(iv);
+
+			}
+			return touchingLeft || touchingRight;
+		}
+		else{
+			return false;
+		}
+	}
+	private boolean collisionWithTop(ImageView iv){
+		if(getChildren().contains(iv)) {
+			double topBall = ball.getY() - ball.getCircle().getRadius();
+			double bottomBall = ball.getY() + ball.getCircle().getRadius();
+			double leftBall = ball.getX() - ball.getCircle().getRadius();
+			double rightBall = ball.getX() + ball.getCircle().getRadius();
+
+			double topIV = iv.getY();
+			double botIV = topIV + iv.getImage().getHeight();
+			double leftIV = iv.getX();
+			double rightIV = leftIV + iv.getImage().getWidth();
+			boolean inColumn = ((leftBall >= leftIV && leftBall <= rightIV) || (rightBall <= rightIV && rightBall >= leftIV));
+
+			boolean touchingTop = (bottomBall >= topIV && bottomBall <= botIV) && inColumn;
+			boolean touchingBot = (topBall >= topIV && topBall <= botIV) && inColumn;
+			if (touchingTop || touchingBot) {
+				getChildren().remove(iv);
+				animals.remove(iv);
+
+			}
+			return touchingTop || touchingBot;
+		}
+		else{
+			return false;
+		}
 
 	}
 
@@ -195,10 +345,7 @@ public class GameImpl extends Pane implements Game {
 		ball.updatePosition(deltaNanoTime);
 		double topBall = ball.getY() - ball.getCircle().getRadius();
 		double bottomBall = ball.getY() + ball.getCircle().getRadius();
-		double leftBall=ball.getX()-ball.getCircle().getRadius();
 
-		double leftIV=iv1.getX();
-		double rightIV=leftIV+iv1.getImage().getWidth();
 
 
 		if((collisionWith(paddle,ball) && !collisionInFrame) || topBall<=0 && topBall>=-5){
@@ -208,10 +355,18 @@ public class GameImpl extends Pane implements Game {
 		} else if(bottomBall>=HEIGHT){
 			ball.setVy(-ball.getVy());
 			numberOfBottomHits++;
-		} else if(collisionWithSide(iv1)){
-			getChildren().remove(iv1);
-			ball.setVx(-ball.getVx());
-			numberOfAnimalsLeft--;
+		} else if(collisionWithSide(iv1)||collisionWithSide(iv2)||collisionWithSide(iv3)||collisionWithSide(iv4)){
+			//ball.setVx(-ball.getVx());
+
+			System.out.println("collision with side");
+		} else if(collisionWithTop(iv1)||collisionWithTop(iv2)||collisionWithTop(iv3) ||collisionWithTop(iv4)
+				||collisionWithTop(iv5)||collisionWithTop(iv6)||collisionWithTop(iv7)||collisionWithTop(iv8)
+				||collisionWithTop(iv9)||collisionWithTop(iv10)||collisionWithTop(iv11)||collisionWithTop(iv12)
+				||collisionWithTop(iv13)||collisionWithTop(iv14)||collisionWithTop(iv15)||collisionWithTop(iv16)){
+			//ball.setVy(-ball.getVy());
+			ball.setVx((SPEED_FACTOR/Math.sqrt(2)*ball.getVx()));
+			ball.setVy((-SPEED_FACTOR/Math.sqrt(2)*ball.getVy()));
+			System.out.println("collision with top");
 		}
 
 
@@ -225,7 +380,7 @@ public class GameImpl extends Pane implements Game {
 
 		if(numberOfBottomHits==5)
 			return GameState.LOST;
-		else if(numberOfAnimalsLeft==0)
+		else if(animals.size()==0)
 			return GameState.WON;
 		else
 			return GameState.ACTIVE;
